8c609fad6c1159796d7e4472e6e3b25c83b79c0d
GuidoBarzaghi <guidobarzaghi@gmail.com>
2025-07-09 02:23:09 +0200

    added links to README and pre-processing script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Scripts to reproduce the figures of https://doi.org/10.1101/2025.03.31.646464
 
-For more details on SMF data pre-processing and analysis, refer to our previosuly published protocol paper at https://doi.org/10.1038/s41596-021-00630-1
+For more details on SMF data pre-processing and analysis, refer to our previosuly published protocol paper at https://doi.org/10.1038/s41596-021-00630-1 and to this vignette from our R/Bioconductor package https://bioconductor.org/packages/release/bioc/vignettes/SingleMoleculeFootprinting/inst/doc/methylation_calling_and_QCs.html.
--- write_sampleFile.r
+++ write_sampleFile.r
@@ -2,6 +2,6 @@
 
 # this writes a sampleFile.txt in the current working directory using all the bam files contained in the path passed as $1 argument  
 
-echo "FileName  SampleName" > sampleFile.txt
-for file in $(ls $1/*.bam); do echo $file $(echo $file | sed 's/\//\t/g' | rev | cut -f 1 | rev | sed 's/.bam//g'); done | sed 's/ /\t/g' >> sampleFile.txt
+echo "FileName  SampleName" > Can_amplicons_NRF1KD_QuasR_input.txt
+for file in $(ls $1/*.bam); do echo $file $(echo $file | sed 's/\//\t/g' | rev | cut -f 1 | rev | sed 's/.bam//g'); done | sed 's/ /\t/g' >> Can_amplicons_NRF1KD_QuasR_input.txt
 
